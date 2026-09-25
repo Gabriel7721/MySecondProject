f64ae7d143ae73355af42be9c34f7dd2e9a32704
LeHiu2712 <protector2712@gmail.com>
2026-09-25 21:03:04 +0700

    Cap nhat properties HP cho Player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,14 +1,18 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int level = 10;
     public string gender = "male";
+
+    public int hitPoint = 95;
     void Start()
     {
         Debug.Log("Player started!");
         Debug.Log("Player level: " + level);
         Debug.Log("Player gender: " + gender);
+        Debug.Log("Player HP: " + hitPoint);
     }
 
     void Update()
