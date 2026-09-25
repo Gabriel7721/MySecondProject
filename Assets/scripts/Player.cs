9c603f8d80767f637b0333571f8dd3832b8bddae
Kai02122002 <narunguyen@yahoo.com>
2026-09-25 21:00:53 +0700

    Cập nhật tên nhân vật

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,14 +1,17 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int level = 10;
     public string gender = "male";
+    public string name = "Main character";
     void Start()
     {
         Debug.Log("Player started!");
         Debug.Log("Player level: " + level);
         Debug.Log("Player gender: " + gender);
+        Debug.Log("Player name:" + name);
     }
 
     void Update()
