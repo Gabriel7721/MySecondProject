2fe3a0868faad7d30c838bbb022cec92ccb49306
daoducson1999 <sondd.124010126001@vtc.edu.vn>
2026-09-25 20:25:45 +0700

    Cập Nhật HP

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,14 +1,17 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public int level = 10;
     public string gender = "male";
+    public int HP = "100";
     void Start()
     {
         Debug.Log("Player started!");
         Debug.Log("Player level: " + level);
         Debug.Log("Player gender: " + gender);
+        Debug.Log("HP: " + HP);
     }
 
     void Update()
