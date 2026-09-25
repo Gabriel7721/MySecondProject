51fbe56c3a154052c3fa29b162e8a718648d6148
trungnguyen-a <insane2806@gmail.com>
2026-09-25 21:00:40 +0700

    update player height

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,11 +4,13 @@ public class Player : MonoBehaviour
 {
     public int level = 10;
     public string gender = "male";
+    public int height = 1280;
     void Start()
     {
         Debug.Log("Player started!");
         Debug.Log("Player level: " + level);
         Debug.Log("Player gender: " + gender);
+        Debug.Log("Player height: " + height);
     }
 
     void Update()
